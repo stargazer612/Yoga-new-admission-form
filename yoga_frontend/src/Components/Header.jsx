4d359eeab4a283186d8a5ff47b29ed634a075adc
Yashaswa Jain <ishujain18123@gmail.com>
2021-11-13 10:01:06 +0530

    Frontend added

---
+++ yoga_frontend/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css'
+function Header() {
+    return (
+        <div className="yogaHeader">
+            <h>Yoga Classes Admission Form</h>
+        </div>
+    )
+}
+
+export default Header
